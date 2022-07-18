079004fbcf3ab0ca052c797b5c87b63cf6cc8f56
barrowpaul <barrowpaul738@gmail.com>
2022-07-18 15:02:46 +0300

    Added combining functionality

--- classes.py
+++ classes.py
@@ -14,6 +14,16 @@ class Item:
         return self.price * self.quantity
 
 
-item1 = Item("Phone", 23.77, 12)
+item1 = Item("Phone", 1250, 4)
+item2 = Item("Macbook", 1800, 2)
 
-print(f"Item: {item1.name}\nPrice: ${item1.price}\nQuantity: {item1.quantity}\nTotal cost: ${item1.calculate_total_cost()}")
+print(f"Item 1: {item1.name}\nPrice: ${item1.price}\nQuantity: {item1.quantity}"
+      f"\nTotal cost: ${item1.calculate_total_cost()}\n" + "*" * 20)
+
+print(f"Item 2: {item2.name}\nPrice: ${item2.price}\nQuantity: {item2.quantity}"
+      f"\nTotal cost: ${item2.calculate_total_cost()}\n" + "*" * 20)
+
+print(f"Total Items: {item1.quantity + item2.quantity},"
+      f" Total price: ${item1.calculate_total_cost() + item2.calculate_total_cost()}")
+
+
