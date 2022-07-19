c5ec723001df6cc068d254ac3c07bc0a4855e436
barrowpaul <barrowpaul738@gmail.com>
2022-07-19 11:11:21 +0300

    When to use static methods, and when to use class methods

---
+++ when_to_use_class_and_static_methods.py
@@ -0,0 +1,14 @@
+class Item:
+    @staticmethod
+    def is_integer(num):
+        """
+        This should do something that has a relationship with the class, but not something that must be
+        unique to each instance!
+        """
+
+    @classmethod
+    def instantiate_from_something(cls):
+        """
+        This should also do something that has a relationship with the class, but usually, those are to
+        manipulate different structures of data to instantiate objects like we have done on the CSV
+        """
