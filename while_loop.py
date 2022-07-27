44894113014944ff35e37abcf0849cb4501032e5
barrowpaul <barrowpaul738@gmail.com>
2022-07-27 17:56:48 +0300

    Working with the while loop

---
+++ while_loop.py
@@ -0,0 +1,8 @@
+user_value = input('Enter a number ')
+
+while int(user_value) < 10:
+    user_value = input('The number is less than 10, try again! ')
+
+print(f"Alright, your number {user_value} was greater than 10!")
+
+
