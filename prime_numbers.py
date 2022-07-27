de25067cff2634cfd5d09e48ee0f0bbf1fa9d53e
barrowpaul <barrowpaul738@gmail.com>
2022-07-27 19:24:50 +0300

    Kienyeji solving for prime numbers

---
+++ prime_numbers.py
@@ -0,0 +1,16 @@
+# Kienyeji solving for prime numbers
+not_prime_numbers = []
+for numbers in range(100):
+    for divisor in range(2, numbers):
+        if numbers % divisor == 0:
+            not_prime_numbers.append(numbers)
+
+all_numbers = []
+for i in range(2, 100):
+    all_numbers.append(i)
+
+print(set(all_numbers) - set(not_prime_numbers))
+
+# dividend = int(input('Please enter a number'))
+#
+# for divisor in range(2, dividend)
