60007f7d87ba8326616cee65e22ed8d8f9db0761
barrowpaul <barrowpaul738@gmail.com>
2022-07-28 20:12:12 +0300

    Added a simple dictionary

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,9 @@ names = ['kev', 'jan', 'line', 'ken', 'juanpa', 'zurita']
 print(names)
 names.sort()
 print(names)
+
+
+# Dictionaries here
+student = {'name': 'John Smith', 'D.o.B': '1999'}
+
+print(student['name'])
