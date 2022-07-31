bff3ddfd505f58e5b72e94f55594e939e69580ca
barrowpaul <barrowpaul738@gmail.com>
2022-07-31 17:41:37 +0300

    Movie lists

--- enumeration.py
+++ enumeration.py
@@ -3,3 +3,25 @@ movie = ('The fast and the furious', 'Jim Someone', 2000000)
 movie_name, director, budget = movie
 
 print(f"For the movie {movie_name}, the director was, {director} and the budget was {budget}")
+
+
+movies = [
+    (
+        "Eternal Sunshine of the Spotless Mind",
+        "Michel Gondry",
+        2004
+    ),
+    (
+        "Memento",
+        "Christopher Nolan",
+        2000
+    ),
+    (
+        "Requiem for a Dream",
+        "Darren Aronofsky",
+        2000
+    )
+]
+
+for title, director, year in movies:
+    print(f"{title} ({year}), by {director}")
