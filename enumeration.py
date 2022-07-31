2f6fe6078164c944f33a3b8ff890ccf6ac501020
barrowpaul <barrowpaul738@gmail.com>
2022-07-31 17:34:29 +0300

    enumaration

---
+++ enumeration.py
@@ -0,0 +1,5 @@
+movie = ('The fast and the furious', 'Jim Someone', 2000000)
+
+movie_name, director, budget = movie
+
+print(f"For the movie {movie_name}, the director was, {director} and the budget was {budget}")
