4deeca3d4f5186433170c2f97e586ecbba6a5ef9
barrowpaul <barrowpaul738@gmail.com>
2022-08-01 18:16:44 +0300

    Worked on difference and symmetric difference

--- sets.py
+++ sets.py
@@ -54,3 +54,4 @@ bundle_1 = {"Resident Evil 3", "Final Fantasy VII", "Cyberpunk 2077"}
 bundle_2 = {"Doom Eternal", "Halo Infinite", "Resident Evil 3"}
 
 print(bundle_1.symmetric_difference(bundle_2))
+
