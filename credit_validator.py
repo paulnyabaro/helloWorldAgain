e49ef79a4ec606b3c01a80bcf94e181cde81e896
barrowpaul <barrowpaul738@gmail.com>
2022-08-01 15:21:17 +0300

    Working on credit card validator

---
+++ credit_validator.py
@@ -0,0 +1,5 @@
+# Credit card number validator using Luhn algorithm
+
+credit_card_number = input("Enter the credit card number: ")
+
+print(f"Your credit number is {credit_card_number} and is invalid.")
