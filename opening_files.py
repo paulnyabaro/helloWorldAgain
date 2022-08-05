75312954dcb0f9d48c18add2c183249a44d3cac8
barrowpaul <barrowpaul738@gmail.com>
2022-08-05 19:39:51 +0300

    Specifying the mode of opening the file

--- opening_files.py
+++ opening_files.py
@@ -2,3 +2,10 @@ example = open('example.txt')
 # example = open('./example.txt') We could also write it in this format, and it will be okay
 print(example.read())
 example.close()
+
+# Opening a file for reading
+# example = open('example.txt', 'r')
+# example = open('example.txt', mode='r')
+
+# Opening files for writing
+example = open('example.txt', 'w')
