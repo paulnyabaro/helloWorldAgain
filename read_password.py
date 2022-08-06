8e92aa01b26149216012ec69df12785e4c9ee262
barrowpaul <barrowpaul738@gmail.com>
2022-08-06 13:10:14 +0300

    Reading a password file

---
+++ read_password.py
@@ -0,0 +1,12 @@
+read_password = open('password.txt', 'r')
+password = read_password.read()
+
+user_password = input('Please enter you password to proceed ')
+if user_password == password:
+    print('Access granted')
+
+elif user_password == '12345':
+    print('This is what idiots use as password')
+
+else:
+    print('Access denied')
