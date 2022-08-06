46319947550303deb49bde3529f6146cebba24e0
barrowpaul <barrowpaul738@gmail.com>
2022-08-06 13:32:28 +0300

    Created and interger dividor

---
+++ rem.py
@@ -0,0 +1,4 @@
+divisor = int(input('Enter the divisor '))
+divider = int(input('Enter the divider '))
+
+print(f'{divisor} divided by {divider} is {divisor // divider} remainder {divisor % divider}')
