dbfb3a828b8b2d86fb5dc50b125cfffac992c2cd
barrowpaul <barrowpaul738@gmail.com>
2022-08-10 19:52:43 +0300

    Replacing the old values with the new ones within the list

--- comprehensions.py
+++ comprehensions.py
@@ -10,3 +10,7 @@ print(processed_names)
 # Using comprehension
 processed_names_2 = [name.title() for name in names]
 print(processed_names_2)
+
+# Replacing the old list with the new one
+names = [name.title() for name in names]
+print(names)
