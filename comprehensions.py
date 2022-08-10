14c5c4c8ed4094640f45738610fe2d89f95ad9f1
barrowpaul <barrowpaul738@gmail.com>
2022-08-10 19:51:05 +0300

    Using list comprehension one

--- comprehensions.py
+++ comprehensions.py
@@ -1,7 +1,12 @@
 names = ['James', 'Lilian', 'Kelly', 'EVERLYNE', 'pAULinE']
 processed_names = []
 
+# The traditional way
 for name in names:
     processed_names.append(name.title())
 
 print(processed_names)
+
+# Using comprehension
+processed_names_2 = [name.title() for name in names]
+print(processed_names_2)
