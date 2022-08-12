dbae3beda30840172719c2c0bc306970742e10a1
barrowpaul <barrowpaul738@gmail.com>
2022-08-12 22:07:22 +0300

    Working with dictionaries in functions

---
+++ first_class_functions.py
@@ -0,0 +1,12 @@
+students = [
+    {"name": "Hannah", "grade_average": 83},
+    {"name": "Charlie", "grade_average": 91},
+    {"name": "Peter", "grade_average": 85},
+    {"name": "Rachel", "grade_average": 79},
+    {"name": "Lauren", "grade_average": 92}
+]
+
+
+def get_grade_average(student):
+    return student["grade_average"]
+
