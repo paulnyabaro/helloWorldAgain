9a3f38da8f930cfec9c676eae186d0ab6512fbea
barrowpaul <barrowpaul738@gmail.com>
2022-08-22 10:59:48 +0300

    Adding iterables

--- args_and_kwargs.py
+++ args_and_kwargs.py
@@ -1,5 +1,12 @@
 def mult(*args):
-    return args[0] * args[1]
+    return args[0] * args[1] * args[2]
 
 
-print(mult(4, 32))
+print(mult(4, 32, 324))
+
+
+def add_nums(*args):
+    return sum(args)
+
+
+print(add_nums(1, 23, 43, 342, 432, 2, 12, 42))
