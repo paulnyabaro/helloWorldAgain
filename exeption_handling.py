96c6652052d97a4e5429f3df26db1e2a5f948c59
barrowpaul <barrowpaul738@gmail.com>
2022-08-24 20:14:55 +0300

    Using more that one except clouse

--- exeption_handling.py
+++ exeption_handling.py
@@ -4,3 +4,6 @@ while True:
         break
     except ValueError:
         print('You did not enter a valid number, please try again!')
+
+    except TypeError:
+        print('The entered value is not a digit')
