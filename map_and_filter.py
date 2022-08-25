31a18f27cc95b9702b05a4419cfcc9ceab6ff60a
barrowpaul <barrowpaul738@gmail.com>
2022-08-25 21:11:17 +0300

    Using * to print iterables

--- map_and_filter.py
+++ map_and_filter.py
@@ -4,6 +4,9 @@ def square(num):
 
 numbers = [1, 3, 4, 6, 3, 9]
 squared_numbers = map(square, numbers)
+#
+# for number in squared_numbers:
+#     print(number)
 
-for number in squared_numbers:
-    print(number)
+# Using * to upack the iterables
+print(*squared_numbers, sep=', ')
