420ca5177d53d03425d166d494104682b13f3573
barrowpaul <barrowpaul738@gmail.com>
2022-08-25 21:25:34 +0300

    Using none with filter

--- map_and_filter.py
+++ map_and_filter.py
@@ -16,3 +16,10 @@ print(*squared_numbers, sep=', ')
 numbers = [1, 56, 3, 5, 24, 19, 88, 37]
 even_numbers = filter(lambda number: number % 2 == 0, numbers)
 print(*even_numbers, sep=', ')
+
+
+# Using None with filter
+values = [0, "Hello", [], {}, 435, -4.2, ""]
+truthy_values = filter(None, values)
+
+print(*truthy_values, sep=", ")
