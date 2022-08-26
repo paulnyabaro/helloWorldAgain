3b20cc65db8ca33a620394c8e554053d0f87be5e
barrowpaul <barrowpaul738@gmail.com>
2022-08-26 20:18:57 +0300

    using a function from the imported file

--- spliting_to_multiple_files.py
+++ spliting_to_multiple_files.py
@@ -1,3 +1,6 @@
-import to_be_imported
+import to_be_imported as ti
 
 print('This is print from this file')
+
+date_of_birth = input('What is your date of birth?')
+print(f'You are {ti.calculate_age(date_of_birth)} years old')
--- to_be_imported.py
+++ to_be_imported.py
@@ -1,1 +1,5 @@
 print('This is print from another file')
+
+
+def calculate_age(dob):
+    return 2022 - int(dob)
