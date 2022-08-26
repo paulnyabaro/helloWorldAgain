ac1d04e8ad8461dc3925bb4ab5f2c7b5c2dfa4ae
barrowpaul <barrowpaul738@gmail.com>
2022-08-26 20:31:12 +0300

    filling remaining spaces with characters

--- spliting_to_multiple_files.py
+++ spliting_to_multiple_files.py
@@ -4,3 +4,11 @@ print('This is print from this file')
 
 date_of_birth = input('What is your date of birth?')
 print(f'You are {ti.calculate_age(date_of_birth)} years old')
+
+
+# Testing random code from twitter
+align = '<'
+fill_char = '*'
+width = 25
+s = 'P'
+print(f'{s:{fill_char}{align}{width}}')
