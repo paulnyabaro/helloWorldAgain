e7759528a581cb064dc531778097c0335350f1c8
barrowpaul <barrowpaul738@gmail.com>
2022-08-29 15:20:20 +0300

    Using iter function to convert a list to iterator

--- gym.py
+++ gym.py
@@ -5,3 +5,14 @@ exercises = itertools.cycle(['Push ups', 'Pull ups', 'Squats', 'Handstand Push u
 
 for days in range(1, 30):
     print(f'Day {days}: ({next(gym_days)}) do {next(exercises)} exercise')
+
+
+# Converting a list to an iterator
+numbers = [1, 2, 3, 4, 5, 6]
+numbers_iter = iter(numbers)
+
+print(next(numbers_iter))
+print(next(numbers_iter))
+print(next(numbers_iter))
+print(next(numbers_iter))
+print(next(numbers_iter))
