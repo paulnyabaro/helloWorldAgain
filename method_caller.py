3e07949ca607a00d9283663b437e071b3dc15449
barrowpaul <barrowpaul738@gmail.com>
2022-08-29 14:49:51 +0300

    Using next on iterables

--- method_caller.py
+++ method_caller.py
@@ -3,3 +3,11 @@ from operator import methodcaller
 words = ["anaconda", "peach", "gravity", "cattle", "anime", "addition"]
 a_words = filter(methodcaller("startswith", "a"), words)
 print(a_words)
+
+# for word in a_words:
+#     print(word)
+#
+
+# Using next functions
+first_word = next(a_words)
+print(first_word)
