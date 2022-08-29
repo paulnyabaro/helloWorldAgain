26fab9dcec37a95ae71660284ee72e53f38df6b6
barrowpaul <barrowpaul738@gmail.com>
2022-08-29 17:38:43 +0300

    Completed the collatz number sequence

--- collatz.py
+++ collatz.py
@@ -2,7 +2,10 @@ def collatz(num):
     while num != 1:
         if num % 2 == 0:
             num = num / 2
+            print(int(num))
         else:
             num = (num * 3) + 1
+            print(int(num))
 
 
+collatz(300)
