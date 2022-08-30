555fddc3471c4e49eb98e4867eb1ef6f6a943724
barrowpaul <barrowpaul738@gmail.com>
2022-08-30 20:01:08 +0300

    Using the setdefault method on dictionaries

--- greetings.py
+++ greetings.py
@@ -1,2 +1,7 @@
-name = input('What is your name? ') or 'World'
+name = input('What is your name? ').strip().title() or 'World'
 print(f'Hello {name}')
+
+
+student_data = {'name': 'Jolie', 'age': 12, 'favorite color': 'pink'}
+print(student_data.setdefault('favorite teacher', 'Eveline'))
+print(student_data.setdefault('age', 10))
