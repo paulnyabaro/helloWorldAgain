bb6a2cddbe31a6c57d1abcb8b4ec75e99536ffe5
barrowpaul <barrowpaul738@gmail.com>
2022-08-30 20:05:50 +0300

    Counting characters in a sentence

--- greetings.py
+++ greetings.py
@@ -6,3 +6,11 @@ student_data = {'name': 'Jolie', 'age': 12, 'favorite color': 'pink'}
 print(student_data.setdefault('favorite teacher', 'Eveline'))
 print(student_data.setdefault('favorite teacher', 'Jane'))
 print(student_data.setdefault('age', 10))
+
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+print(count)
