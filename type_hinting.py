be3458106ceed70411b48e8500ea9f7076fe6b6e
barrowpaul <barrowpaul738@gmail.com>
2022-08-31 15:53:15 +0300

    Annotating functions

--- type_hinting.py
+++ type_hinting.py
@@ -17,5 +17,19 @@ names_three: List[Union[str, int]] = ["x", 13, "camel", 0]
 
 # Working with tuples
 movie: Tuple[str, str, int] = ("Toy Story 3", "Lee Unkrich", 2010)
-
 print(names_two)
+
+# Annotating functions
+Movie = Tuple[str, str, int]
+
+def show_movies(movies: List[Movie]):
+    for title, director, year in movies:
+        print(f"{title} ({year}), by {director}")
+
+movies: List[Movie] = [
+    ("Finding Nemo", "Andrew Stanton", 2005),
+    ("Inside Out", "Pete Docter", 2015),
+    ("Toy Story 3", "Lee Unkrich", 2010)
+]
+
+show_movies(movies)
