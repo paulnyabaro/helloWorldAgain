7f1bb71c3679e2cee84c6405c8def0bb4f803521
barrowpaul <barrowpaul738@gmail.com>
2022-08-31 15:48:52 +0300

    Shortened the imports

--- type_hinting.py
+++ type_hinting.py
@@ -1,8 +1,4 @@
-from typing import Union
-from typing import Any
-from typing import List
-from typing import Tuple
-
+from typing import Union, Any, List, Tuple
 
 name: str = "Paul"
 age: int = 25
