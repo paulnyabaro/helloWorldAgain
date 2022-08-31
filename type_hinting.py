f79780e055348d1b2272ac9195cd3f3e1186a3f4
barrowpaul <barrowpaul738@gmail.com>
2022-08-31 15:32:53 +0300

    Using Union and Any

--- type_hinting.py
+++ type_hinting.py
@@ -1,6 +1,11 @@
+from typing import Union
+from typing import Any
+
+
 name: str = "Paul"
 age: int = 25
-height_cm: float = 170
+height_cm: Union[int, float] = 170
+challenges_in_learning_python: Any = "Not much really"
 loves_python: bool = True
 
 print(height_cm)
