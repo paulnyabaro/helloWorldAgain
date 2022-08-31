d61a832f6637e9d4b46be6c972fba12ce24e5d0d
barrowpaul <barrowpaul738@gmail.com>
2022-08-31 16:28:00 +0300

    Using ljust, rjust and center to show lists in cmd

--- string_methods.py
+++ string_methods.py
@@ -2,3 +2,13 @@ string_name = 'This is the name'
 print(string_name.rjust(50, '*'))
 print(string_name.ljust(50, '-'))
 print(string_name.center(50, '+'))
+
+
+def printPicnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+printPicnic(picnicItems, 12, 5)
+printPicnic(picnicItems, 20, 6)
