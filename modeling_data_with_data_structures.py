e39d84f902236631b5901621b409c25533b46314
barrowpaul <barrowpaul738@gmail.com>
2022-08-31 14:00:34 +0300

    Working with pandas

--- modeling_data_with_data_structures.py
+++ modeling_data_with_data_structures.py
@@ -20,3 +20,4 @@ print(' - Apple Pies ' + str(total_brought(all_guests, 'apple pies')))
 
 
 
+
---
+++ working_with_pandas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+movies = {
+    "title": ("Inception", "Pirates of the Caribbean: The Curse of the Black Pearl"),
+    "director": ("Christopher Nolan", "Gore Verbinski"),
+    "year": (2010, 2003)
+}
+
+df = pd.DataFrame(movies)
+
+print(df)
