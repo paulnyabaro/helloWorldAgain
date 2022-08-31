5eee94e36df6705971f2ab68487ae536cc95df37
barrowpaul <barrowpaul738@gmail.com>
2022-08-31 20:07:54 +0300

    Breaking down program into simple one using regular expressions

--- regular_expressions.py
+++ regular_expressions.py
@@ -3,3 +3,8 @@ import re
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('My number is 415-555-4242.')
 print('Phone number found: ' + mo.group())
+
+# mo is matching objects
+a = True
+b = False
+print(a or a and b and a)
