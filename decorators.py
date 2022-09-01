80d3456abf9955c1d4dad09123d2171310f8d152
barrowpaul <barrowpaul738@gmail.com>
2022-09-01 05:27:18 +0300

    A much better way of using decorators

--- decorators.py
+++ decorators.py
@@ -1,3 +1,27 @@
+# from typing import Callable
+#
+# def example_decorator(func: Callable) -> Callable:
+#     def inner():
+#         print(f"Now calling {func.__name__}...")
+#         func()
+#         print(f"{func.__name__} has ended.")
+#
+#     return inner
+#
+# def example_decorator_2(func: Callable) -> Callable:
+#     def inner_2():
+#         print(f"Now calling {func.__name__}...")
+#         func()
+#         print(f"{func.__name__} has ended.")
+#
+#     return inner_2
+#
+# def greeter():
+#     print("Hello!")
+#
+# greeter = example_decorator(greeter)
+# greeter()
+
 from typing import Callable
 
 def example_decorator(func: Callable) -> Callable:
@@ -8,16 +32,8 @@ def example_decorator(func: Callable) -> Callable:
 
     return inner
 
-def example_decorator_2(func: Callable) -> Callable:
-    def inner_2():
-        print(f"Now calling {func.__name__}...")
-        func()
-        print(f"{func.__name__} has ended.")
-
-    return inner_2
-
+@example_decorator
 def greeter():
     print("Hello!")
 
-greeter = example_decorator(greeter)
 greeter()
