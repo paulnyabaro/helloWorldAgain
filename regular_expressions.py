69c51b2260f1081e8b0114b70649a029891d09ef
barrowpaul <barrowpaul738@gmail.com>
2022-09-01 05:46:58 +0300

    Regular expressions grouping

--- regular_expressions.py
+++ regular_expressions.py
@@ -8,3 +8,11 @@ print('Phone number found: ' + mo.group())
 a = True
 b = False
 print(a or a and b and a)
+
+# Regular expressions grouping
+phoneNumRegex_two = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex_two.search('My number is 415-555-4242.')
+print(mo.group(1))
+print(mo.group(2))
+print(mo.group(0))
+print(mo.group())
