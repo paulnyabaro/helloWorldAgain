170229062223738b693a5dcada157178b25caa95
barrowpaul <barrowpaul738@gmail.com>
2022-09-01 05:24:23 +0300

    Working with decorators

--- decorators.py
+++ decorators.py
@@ -7,3 +7,17 @@ def example_decorator(func: Callable) -> Callable:
         print(f"{func.__name__} has ended.")
 
     return inner
+
+def example_decorator_2(func: Callable) -> Callable:
+    def inner_2():
+        print(f"Now calling {func.__name__}...")
+        func()
+        print(f"{func.__name__} has ended.")
+
+    return inner_2
+
+def greeter():
+    print("Hello!")
+
+greeter = example_decorator(greeter)
+greeter()
