7f6e95eb164b03a6c1afd8ba182142f402bed1ed
barrowpaul <barrowpaul738@gmail.com>
2022-09-01 10:13:41 +0300

    Regular expressions summary

--- regular_expressions.py
+++ regular_expressions.py
@@ -27,3 +27,22 @@ print(mo_three.group())
 # (wo){4} or (wo){2,4} or (wo){4,} or (wo){,4} for repeting the pattern the number of times
 # Using search and findall method
 # ^ begins with
+
+
+# SUMMARY
+"""The ? matches zero or one of the preceding group.
+The * matches zero or more of the preceding group.
+The + matches one or more of the preceding group.
+The {n} matches exactly n of the preceding group.
+The {n,} matches n or more of the preceding group.
+The {,m} matches 0 to m of the preceding group.
+The {n,m} matches at least n and at most m of the preceding group.
+{n,m}? or *? or +? performs a nongreedy match of the preceding group.
+^spam means the string must begin with spam.
+spam$ means the string must end with spam.
+The . matches any character, except newline characters.
+\d, \w, and \s match a digit, word, or space character, respectively.
+\D, \W, and \S match anything except a digit, word, or space character,
+respectively.
+[abc] matches any character between the brackets (such as a, b, or c).
+[^abc] matches any character that isn’t between the brackets."""
