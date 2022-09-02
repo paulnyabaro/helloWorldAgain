fe5cc37ea793d4e1e32524619caf6e8acb5a41c6
barrowpaul <barrowpaul738@gmail.com>
2022-09-02 07:00:54 +0300

    Drawing objects on the screen

--- using_pygame.py
+++ using_pygame.py
@@ -1,10 +1,18 @@
 import pygame
+from collections import namedtuple
+
+Colour = namedtuple("Colour", ["red", "green", "blue"])
+
+BACKGROUND_COLOUR = Colour(red=36, green=188, blue=168)
+RECTANGLE_COLOUR = Colour(red=255, green=255, blue=255)
 
 pygame.init()
 
 pygame.display.set_caption('My first pygame')
 screen = pygame.display.set_mode([640, 480])
-screen.fill([73, 29, 43])
+screen.fill(BACKGROUND_COLOUR)
+
+pygame.draw.rect(screen, RECTANGLE_COLOUR, [0, 0, 100, 50])
 
 pygame.display.update()
 
