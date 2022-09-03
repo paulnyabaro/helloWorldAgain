6d4a7b4e1877e9a01aa7af313251e440f398dfa2
barrowpaul <barrowpaul738@gmail.com>
2022-09-03 07:49:19 +0300

    Working with zipfile module

--- zipfile_module.py
+++ zipfile_module.py
@@ -2,3 +2,9 @@ import zipfile
 
 exampleZip = zipfile.ZipFile('example.zip')
 exampleZip.namelist()
+spamInfo = exampleZip.getinfo('spam.txt')
+spamInfo.file_size
+spamInfo.compress_size
+'Compressed file is %sx smaller!' % (round(spamInfo.file_size / spamInfo
+.compress_size, 2))
+exampleZip.close()
