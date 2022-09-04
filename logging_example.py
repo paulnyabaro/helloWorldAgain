565ec1bd5a8072f3637857eff6ab2cab557908ab
barrowpaul <barrowpaul738@gmail.com>
2022-09-04 08:25:46 +0300

    Logging to a file

--- logging_example.py
+++ logging_example.py
@@ -1,5 +1,7 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+# Writing to a file
+logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 logging.debug('Start of program')
 def factorial(n):
     logging.debug('Start of factorial(%s%%)' % (n))
