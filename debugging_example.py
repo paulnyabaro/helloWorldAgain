4ba484d16ad27b3793cb6c3b516372023f2f7b4c
barrowpaul <barrowpaul738@gmail.com>
2022-09-04 07:54:20 +0300

    Writing traceback information to a file

--- debugging_example.py
+++ debugging_example.py
@@ -2,7 +2,7 @@ import traceback
 try:
     raise Exception('An error occurred')
 except:
-    errorFile = open('errorLog.txt', 'w')
+    errorFile = open('errorLog.txt', 'a')
     print(errorFile.write(traceback.format_exc()))
     errorFile.close()
     print('The traceback info was written to errorLog')
