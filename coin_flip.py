b3f28aa7e79651f57b903198139874814eb81b96
barrowpaul <barrowpaul738@gmail.com>
2022-09-05 06:58:24 +0300

    Addd halfway results

--- coin_flip.py
+++ coin_flip.py
@@ -2,10 +2,13 @@ import random
 
 heads = 0
 tails = 0
-for i in range(0, 1001):
+for i in range(0, 1000):
     if random.randint(0 ,1):
         heads += 1
     else:
         tails += 1
 
-print(f'Heads came {heads} while tails came {tails}')
+    if i == 499:
+        print(f'Halfway results Heads: {heads} Tails: {tails}')
+
+print(f'Full-time results: {heads} and Tails: {tails}')
