ea389c9af14a5b3cf072e15551239e5224196a1f
barrowpaul <barrowpaul738@gmail.com>
2022-09-07 15:59:13 +0300

    Searching on google

---
+++ searching_on_google.py
@@ -0,0 +1,8 @@
+import requests, bs4, webbrowser
+
+search_term = input('Enter the search term you want: ')
+print('Searching...')
+
+res = requests.get(f'https://google.com/search?q{search_term}')
+res.raise_for_status()
+print(res)
