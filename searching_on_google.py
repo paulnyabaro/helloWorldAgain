c0a8e225bf22fad5feb3c48fe662e477e8793077
barrowpaul <barrowpaul738@gmail.com>
2022-09-07 16:03:09 +0300

    Updated google search format

--- searching_on_google.py
+++ searching_on_google.py
@@ -3,6 +3,7 @@ import requests, bs4, webbrowser
 search_term = input('Enter the search term you want: ')
 print('Searching...')
 
-res = requests.get(f'https://google.com/search?q{search_term}')
+res = requests.get(f'https://google.com/search?q={search_term}')
+webbrowser.open(f'https://google.com/search?q={search_term}')
 res.raise_for_status()
 print(res)
