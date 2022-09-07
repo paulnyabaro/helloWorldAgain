d21df03735609b3bb8d87f4f5f7813bc8c809e4f
barrowpaul <barrowpaul738@gmail.com>
2022-09-07 16:11:37 +0300

    Working on fetching results

--- searching_on_google.py
+++ searching_on_google.py
@@ -7,3 +7,7 @@ res = requests.get(f'https://google.com/search?q={search_term}')
 webbrowser.open(f'https://google.com/search?q={search_term}')
 res.raise_for_status()
 print(res)
+
+search_result = bs4.BeautifulSoup(res.text)
+link_elements = search_result.select('.g')
+print(link_elements)
