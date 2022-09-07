b1dc25d884bd3fe4bb2ee8cd16ba2357df0271f4
barrowpaul <barrowpaul738@gmail.com>
2022-09-07 14:48:40 +0300

    Wring the result into a file

--- using_requests.py
+++ using_requests.py
@@ -1,7 +1,7 @@
 import requests
 
 res = requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
-res.raise_for_status()
+res.raise_for_status() # Always use this to make sure it downloads before you continue
 print(res)
 print(len(res.text))
 print(res.text[:250])
@@ -11,3 +11,8 @@ print(res.text[:250])
 #     res.raise_for_status()
 # except Exception as exc:
 #     print('There was a problem: %s' % (exc))
+
+# Writing to a file
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
