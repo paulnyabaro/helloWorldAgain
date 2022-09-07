560160b413680470d35b4e37d2cfcfa1c9be3f28
barrowpaul <barrowpaul738@gmail.com>
2022-09-07 14:50:54 +0300

    Close the file after writing

--- using_requests.py
+++ using_requests.py
@@ -13,6 +13,7 @@ print(res.text[:250])
 #     print('There was a problem: %s' % (exc))
 
 # Writing to a file
+# the b in wb stands for binary
 playFile = open('RomeoAndJuliet.txt', 'wb')
 for chunk in res.iter_content(100000):
     playFile.write(chunk)
