9b3f7101b470f477974b3747d36a8c96cce70919
barrowpaul <barrowpaul738@gmail.com>
2022-09-07 14:44:20 +0300

    Hangling exceptions that results

--- using_requests.py
+++ using_requests.py
@@ -5,3 +5,9 @@ res.raise_for_status()
 print(res)
 print(len(res.text))
 print(res.text[:250])
+
+# using the try and except to handle errors
+# try:
+#     res.raise_for_status()
+# except Exception as exc:
+#     print('There was a problem: %s' % (exc))
