38265d98b5ff900d1ed5449dc4e4e0952eeee4c0
barrowpaul <barrowpaul738@gmail.com>
2022-09-08 18:24:17 +0300

    Will fix later this program

--- web_scraping_two.py
+++ web_scraping_two.py
@@ -4,11 +4,11 @@ url = 'http://xkcd.com'
 os.makedirs('xkcd', exist_ok=True)
 
 while not url.endswith('#'):
+    # Download the page.
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text)
-
     comicElem = soup.select('#comic img')
     if comicElem == []:
         print('Could not find comic image.')
@@ -18,7 +18,6 @@ while not url.endswith('#'):
         print('Downloading image %s...' % (comicUrl))
         res = requests.get(comicUrl)
         res.raise_for_status()
-
         imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
         for chunk in res.iter_content(100000):
             imageFile.write(chunk)
