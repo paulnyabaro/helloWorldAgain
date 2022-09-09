cf06a7fcf756dad447ac5ecf46260ee159d0fb66
barrowpaul <barrowpaul738@gmail.com>
2022-09-09 20:22:23 +0300

    Using PyPDF2 module to merge pdfs

---
+++ pdf_merger.py
@@ -0,0 +1,11 @@
+# from PyPDF2 import PdfMerger
+#
+# pdfs = ['one.pdf','two.pdf', 'three.pdf']
+#
+# merger = PdfMerger()
+#
+# for pdf in pdfs:
+#     merger.append(pdf)
+#
+# merger.write('newfile.pdf')
+# merger.close()
