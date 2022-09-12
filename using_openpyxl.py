14a119a8c3efdf8bacf8608bf55cb53c4015b4d7
barrowpaul <barrowpaul738@gmail.com>
2022-09-12 06:11:00 +0300

    Another deprecated function

--- using_openpyxl.py
+++ using_openpyxl.py
@@ -3,13 +3,14 @@ import openpyxl
 # from openpyxl.cell import get_column_letter, column_index_from_string
 
 wb = openpyxl.load_workbook('example.xlsx')
-print(wb.get_sheet_names()) # Deprecated function
+# print(wb.get_sheet_names()) # Deprecated function
 print(wb.sheetnames) # The new function
 
 # get_column_letter(900)
 
 # Getting rows and columns from the sheets
-sheet = wb.get_sheet_by_name('Sheet1')
+# sheet = wb.get_sheet_by_name('Sheet1') #Deprecated
+sheet = wb['Sheet1']
 tuple(sheet['A1':'C3'])
 for rowOfCellObjects in sheet['A1':'C3']:
     for cellObj in rowOfCellObjects:
