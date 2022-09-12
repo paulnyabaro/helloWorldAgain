e13f92e0120ae09c22cf10ff4bcb599c9728257f
barrowpaul <barrowpaul738@gmail.com>
2022-09-12 06:01:00 +0300

    Converting between column letters and numbers

--- using_openpyxl.py
+++ using_openpyxl.py
@@ -1,5 +1,9 @@
 import openpyxl
+# Converting between column letters and numbers
+# from openpyxl.cell import get_column_letter, column_index_from_string
 
 wb = openpyxl.load_workbook('example.xlsx')
 print(wb.get_sheet_names()) # Deprecated function
 print(wb.sheetnames) # The new function
+
+# get_column_letter(900)
