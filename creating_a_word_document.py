077f46385d75abb282b24c560b9275991ff91068
barrowpaul <barrowpaul738@gmail.com>
2022-09-14 07:51:43 +0300

    Adding titles

--- creating_a_word_document.py
+++ creating_a_word_document.py
@@ -3,7 +3,7 @@ doc = docx.Document()
 doc.add_paragraph('Hello world again!')
 
 # Adding other paragraphs
-paraObj1 = doc.add_paragraph('This is a second paragraph.')
+paraObj1 = doc.add_paragraph('This is a second paragraph.', 'Title') # Adding title
 paraObj2 = doc.add_paragraph('This is a yet another paragraph.')
 paraObj1.add_run(' This text is being added to the second paragraph.')
 doc.save('helloworld.docx')
