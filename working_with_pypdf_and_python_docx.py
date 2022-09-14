068feeb6e71236eadba69b3bbceeb6406c78f1eb
barrowpaul <barrowpaul738@gmail.com>
2022-09-14 06:28:46 +0300

    Adding important comments to show page numbering

--- working_with_pypdf_and_python_docx.py
+++ working_with_pypdf_and_python_docx.py
@@ -2,5 +2,6 @@ import PyPDF2
 pdfFileObj = open('example_pdf.pdf', 'rb') # Read in binary mode as we write in binary mode
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 print(pdfReader.numPages)
-pageObj = pdfReader.getPage(0) # The first page is 0 and the second 1
+pageObj = pdfReader.getPage(0) # The first page is 0 and the second 1 PyPDF2 uses a zero-based index for getting pages
+
 print(pageObj.extractText())
