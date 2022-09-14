517e0bc75535ce5cb4b44e7aba4fcac0f8c6c2e0
barrowpaul <barrowpaul738@gmail.com>
2022-09-14 07:54:05 +0300

    Different heading levels

--- creating_a_word_document.py
+++ creating_a_word_document.py
@@ -4,7 +4,7 @@ doc.add_paragraph('Hello world again!')
 
 # Adding other paragraphs
 paraObj1 = doc.add_paragraph('This is a second paragraph.', 'Title') # Adding title
-# paraObj1 = doc.add_paragraph('This is a second paragraph.', '0') # Different heading levels 0 - 4
+# paraObj1 = doc.add_paragraph('This is a second paragraph.', '0') # 0 is title 1 - 4 heading levels
 paraObj2 = doc.add_paragraph('This is a yet another paragraph.')
 paraObj1.add_run(' This text is being added to the second paragraph.')
 doc.save('helloworld.docx')
