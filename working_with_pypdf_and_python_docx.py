b5761d02f7f76524b832add2d521da456a8fb159
barrowpaul <barrowpaul738@gmail.com>
2022-09-14 06:24:58 +0300

    Selecting page and extracting text from it;

--- working_with_pypdf_and_python_docx.py
+++ working_with_pypdf_and_python_docx.py
@@ -2,3 +2,5 @@ import PyPDF2
 pdfFileObj = open('example_pdf.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 print(pdfReader.numPages)
+pageObj = pdfReader.getPage(0)
+print(pageObj.extractText())
