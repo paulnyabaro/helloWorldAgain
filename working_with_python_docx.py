2b0bd3d7105d26bfbc0be660283e7e70bda4f8c2
barrowpaul <barrowpaul738@gmail.com>
2022-09-14 07:35:48 +0300

    How to indent on word documnts

--- working_with_python_docx.py
+++ working_with_python_docx.py
@@ -2,3 +2,6 @@ import docx
 doc = docx.Document('example_doc.docx')
 print(len(doc.paragraphs))
 print(doc.paragraphs[0].text)
+
+# Indentation in word documents
+# fullText.append('    ' + para.text)
