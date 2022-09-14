7e27977596703a3263d3e998ebbe43f20d03879b
barrowpaul <barrowpaul738@gmail.com>
2022-09-14 07:48:45 +0300

    Updating Word document with additional content

--- creating_a_word_document.py
+++ creating_a_word_document.py
@@ -1,4 +1,9 @@
 import docx
 doc = docx.Document()
-doc.add_paragraph('Hello world!')
+doc.add_paragraph('Hello world again!')
+
+# Adding other paragraphs
+paraObj1 = doc.add_paragraph('This is a second paragraph.')
+paraObj2 = doc.add_paragraph('This is a yet another paragraph.')
+paraObj1.add_run(' This text is being added to the second paragraph.')
 doc.save('helloworld.docx')
