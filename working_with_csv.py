a7d87f7e8478c18e5182843052a26c4aa12d0b20
barrowpaul <barrowpaul738@gmail.com>
2022-09-15 06:21:47 +0300

    Printing items using the for loop

--- working_with_csv.py
+++ working_with_csv.py
@@ -2,6 +2,9 @@ import csv
 
 example_file = open('iris.csv')
 example_reader = csv.reader(example_file)
+
+for row in example_reader:
+    print('Row #' + str(example_reader.line_num) + ' ' + str(row))
 example_data = list(example_reader)
-print(example_data)
-print(example_data[0][0])
+# print(example_data)
+# print(example_data[0][0])
