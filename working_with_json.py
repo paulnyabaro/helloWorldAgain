c03c63c0923bc1b01e121c5f0cd27b785036847e
barrowpaul <barrowpaul738@gmail.com>
2022-09-15 07:24:25 +0300

    Loading the data using json.loads

--- working_with_json.py
+++ working_with_json.py
@@ -5,7 +5,7 @@ import json, requests
 # json_data_in_python = json.loads('string_of_json_data.json')
 # print(json_data_in_python)
 
-url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3'
+url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
 response = requests.get(url)
 response.raise_for_status()
 
@@ -13,3 +13,11 @@ response.raise_for_status()
 # And finally print it using pprint.pprint()
 
 weather_data = json.loads(response.text)
+print('Current weather in %s:' % (location))
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Tomorrow:')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Day after tomorrow:')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
