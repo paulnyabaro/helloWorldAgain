89521844103fe4a1aedbf03953444ed3e07b67c4
barrowpaul <barrowpaul738@gmail.com>
2022-09-15 07:14:23 +0300

    Fetching data from a url

--- working_with_json.py
+++ working_with_json.py
@@ -1,7 +1,10 @@
-import json
+import json, requests, sys
 
 # string_of_json_data = open('string_of_json_data.json')
 #
 # json_data_in_python = json.loads('string_of_json_data.json')
 # print(json_data_in_python)
 
+url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3'
+response = requests.get(url)
+response.raise_for_status()
