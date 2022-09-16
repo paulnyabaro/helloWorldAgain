9f72ee6cfe3f382f2ecb1167e5dd68755b4a4ef4
barrowpaul <barrowpaul738@gmail.com>
2022-09-16 07:07:40 +0300

    Starting other programs

--- working_with_threading.py
+++ working_with_threading.py
@@ -11,3 +11,6 @@ def take_a_nap():
 thread_obj = threading.Thread(target=take_a_nap)
 thread_obj.start()
 print('End of the program')
+
+# Starting other programs
+# Popen() function in the built-in subprocess module starts other programs
