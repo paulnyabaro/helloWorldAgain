5b462db6fca5fb5cd4510547a309c4dd96479837
barrowpaul <barrowpaul738@gmail.com>
2022-09-16 05:00:25 +0300

    Getting the processing time of a function

--- working_with_time.py
+++ working_with_time.py
@@ -3,3 +3,16 @@ import time
 print(time.time())
 
 #The result is how many seconds have passed between the Unix epoch and the moment time.time() was called
+
+# Getting time difference
+def calcProd():
+    # Calculate the product of the first 100,000 numbers.
+    product = 1
+    for i in range(1, 100000):
+        product = product * i
+    return product
+startTime = time.time()
+prod = calcProd()
+endTime = time.time()
+print('The result is %s digits long.' % (len(str(prod))))
+print('Took %s seconds to calculate.' % (endTime - startTime))
