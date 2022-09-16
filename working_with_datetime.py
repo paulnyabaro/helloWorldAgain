5e48bd877fca9e82a1f2ab098bfe08a5a55961ba
barrowpaul <barrowpaul738@gmail.com>
2022-09-16 05:59:54 +0300

    Timedelta

--- working_with_datetime.py
+++ working_with_datetime.py
@@ -17,3 +17,8 @@ print(f'Time passed since I created this code: {datetime.datetime.now() - dt}')
 # converting from timestamp
 print(datetime.datetime.fromtimestamp(1000000000))
 print(datetime.datetime.fromtimestamp(time.time()))
+
+# Timedelta
+delta = datetime.timedelta(days=11, hours=10, minutes=9, seconds=8)
+print(delta.days, delta.seconds, delta.microseconds)
+delta.total_seconds()
