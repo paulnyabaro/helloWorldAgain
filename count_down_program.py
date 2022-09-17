ff7bb631cbbeec786ba410c377d8251af4c60dfa
barrowpaul <barrowpaul738@gmail.com>
2022-09-17 19:30:28 +0300

    Trying to play sound after time has elapsed

--- count_down_program.py
+++ count_down_program.py
@@ -1,9 +1,10 @@
 import time, subprocess
-timeLeft = 60
+
+timeLeft = 2
 while timeLeft > 0:
     print(timeLeft)
     time.sleep(1)
     timeLeft -= 1
 else:
-    subprocess.Popen(['start', 'alarm.wav'])
-    print('Time is up')
+    subprocess.Popen(['start', 'alarm.wav'],shell=True)
+    # print('Time is up')
