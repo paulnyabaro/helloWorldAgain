f6ded7b841a868862acfd1ffd0f21674f71a37b0
barrowpaul <barrowpaul738@gmail.com>
2022-09-19 21:18:52 +0300

    Some issue with pyzmail that I will fix later

--- deleting_email.py
+++ deleting_email.py
@@ -3,6 +3,7 @@ imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
 imapObj.login('my_email_address@gmail.com', 'MY_SECRET_PASSWORD')
 imapObj.select_folder('INBOX', readonly=True) UIDs = imapObj.search(['SINCE 05-Jul-2014'])
 rawMessages = imapObj.fetch([40041], ['BODY[]', 'FLAGS'])
-import pyzmail
-message = pyzmail.PyzMessage.factory(rawMessages[40041]['BODY[]'])
-message.get_subject()
+# import pyzmail
+# message = pyzmail.PyzMessage.factory(rawMessages[40041]['BODY[]'])
+# message.get_subject()
+#  pyzmail has issue installing
