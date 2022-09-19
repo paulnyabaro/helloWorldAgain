030c8c076df8971a4f7a0e95b63fdbe143270048
barrowpaul <barrowpaul738@gmail.com>
2022-09-19 21:51:50 +0300

    Sending messages

--- sending_messages.py
+++ sending_messages.py
@@ -1,1 +1,10 @@
 # using the twilio sms
+
+# from twilio.rest import TwilioRestClient
+# accountSID = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# authToken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# twilioCli = TwilioRestClient(accountSID, authToken)
+# myTwilioNumber = '+14955551234'
+# myCellPhone = '+14955558888'
+# message = twilioCli.messages.create(body='Mr. Watson - Come here - I want to see you.',
+#                                     from_=myTwilioNumber, to=myCellPhone)
