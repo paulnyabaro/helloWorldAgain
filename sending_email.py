9536ff37d06d79ef9a23fccebeecadfe90c12e72
barrowpaul <barrowpaul738@gmail.com>
2022-09-19 14:34:31 +0300

    Having issues with SMTPAuthentication

--- sending_email.py
+++ sending_email.py
@@ -1,3 +1,7 @@
 import smtplib
-smtpObj = smtplib.SMTP('smtp.example.com', 587)
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login('bob@example.com', 'MY_SECRET_PASSWORD')
+smtpObj.sendmail('bob@example.com', 'alice@example.com', 'Subject: So long.\n'
+                    'Dear Alice, so long and thanks for all the fish. Sincerely, Bob')
