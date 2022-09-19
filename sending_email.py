5a76341ea9fee17e12f4f7a942566500de4b4fe2
barrowpaul <barrowpaul738@gmail.com>
2022-09-19 14:43:39 +0300

    Trying SMTP_SSL

--- sending_email.py
+++ sending_email.py
@@ -1,8 +1,9 @@
 import smtplib
-smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+# smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 print(type(smtpObj))
-# smtpObj.ehlo()
-# smtpObj.starttls()
-# smtpObj.login('cit2270312015@mmu.ac.ke', 'Pass1234')
-# smtpObj.sendmail('cit2270312015@mmu.ac.ke', 'barrowspace@gmail.com', 'Subject: Testing smtplib module.\n'
-#                     'Hey there, am tryinto see if this is working')
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login('cit2270312015@mmu.ac.ke', 'Pass1234')
+smtpObj.sendmail('cit2270312015@mmu.ac.ke', 'barrowspace@gmail.com', 'Subject: Testing smtplib module.\n'
+                    'Hey there, am tryinto see if this is working')
