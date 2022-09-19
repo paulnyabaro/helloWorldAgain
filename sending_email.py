a74fc26face68a665b63ec75d55e6f21f85e39ca
barrowpaul <barrowpaul738@gmail.com>
2022-09-19 14:46:39 +0300

    Sending message to the server

--- sending_email.py
+++ sending_email.py
@@ -1,9 +1,14 @@
 import smtplib
-# smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+# Optional if SMTP is not working
+# smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+
+# Determining type
 print(type(smtpObj))
-smtpObj.ehlo()
-smtpObj.starttls()
-smtpObj.login('cit2270312015@mmu.ac.ke', 'Pass1234')
-smtpObj.sendmail('cit2270312015@mmu.ac.ke', 'barrowspace@gmail.com', 'Subject: Testing smtplib module.\n'
-                    'Hey there, am tryinto see if this is working')
+
+# Greetings to the server
+print(smtpObj.ehlo())
+# smtpObj.starttls()
+# smtpObj.login('cit2270312015@mmu.ac.ke', 'Pass1234')
+# smtpObj.sendmail('cit2270312015@mmu.ac.ke', 'barrowspace@gmail.com', 'Subject: Testing smtplib module.\n'
+#                     'Hey there, am tryinto see if this is working')det
