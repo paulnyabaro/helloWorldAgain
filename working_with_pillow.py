1b2e906d283b9b3772e2d8f0b0ee20d332b09211
barrowpaul <barrowpaul738@gmail.com>
2022-09-20 17:59:20 +0300

    Copy pasting on another image

--- working_with_pillow.py
+++ working_with_pillow.py
@@ -1,7 +1,7 @@
 from PIL import Image
 # example_image = Image.open('example_image_two.png')
 example_image = Image.open('example-image.jpg')
-example_image.show()
+# example_image.show()
 print(example_image)
 print(example_image.size)
 width, height = example_image.size
@@ -22,8 +22,15 @@ im2.save('transparentImage.png')
 # Cropping image
 cropped_image = example_image.crop((355, 345, 565, 560))
 cropped_image.save('cropped-image.jpg')
-cropped_image.show()
+# cropped_image.show()
 
 # Copying image
 copied_image = cropped_image.copy()
 copied_image.save('copied-image.jpg')
+
+# Copying and pasting image on another image
+copy_paste_image = example_image.crop((25, 45, 665, 700))
+im.paste(copy_paste_image, (25, 45))
+im.paste(copy_paste_image, (200, 323))
+copy_paste_image.save('copy_paste_image.jpg')
+copy_paste_image.show()
