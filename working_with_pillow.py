d1ef5492d8604855b0c739970a6c4a6cbfd90a2c
barrowpaul <barrowpaul738@gmail.com>
2022-09-20 17:25:19 +0300

    Getting image format and format description

--- working_with_pillow.py
+++ working_with_pillow.py
@@ -6,4 +6,5 @@ print(example_image)
 print(example_image.size)
 width, height = example_image.size
 print(f'The image dimensions are: height --> {height}, and width --> {width}')
+print(example_image.format)
 print(example_image.format_description)
