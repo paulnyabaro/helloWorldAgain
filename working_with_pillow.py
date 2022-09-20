99cb945d51b4de022c27021be0a3df1b9832f893
barrowpaul <barrowpaul738@gmail.com>
2022-09-20 17:22:05 +0300

    Getting image dimensions

--- working_with_pillow.py
+++ working_with_pillow.py
@@ -2,3 +2,6 @@ from PIL import Image
 example_image = Image.open('example_image_two.png')
 example_image.show()
 print(example_image)
+print(example_image.size)
+width, height = example_image.size
+print(f'The image dimensions are: height --> {height}, and width --> {width}')
