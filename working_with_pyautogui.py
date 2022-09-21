c93f8f3e9b4155a41973f48f1f1526e3307543f0
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 06:23:24 +0300

    Getting the screen size

--- working_with_pyautogui.py
+++ working_with_pyautogui.py
@@ -4,4 +4,6 @@ import pyautogui
 # us pyautogui.PAUSE = 1.5 to give you time to interrupt the process in case of an error
 # PyAutoGUI also has a fail-safe feature. Moving the mouse cursor to the
 # upper-left corner of the screen will cause PyAutoGUI to raise the pyautogui
-# .FailSafeException exception
+# .FailSafeException exception --> disable this feature by pyautogui.FAILSAFE = False
+
+print(pyautogui.size())
