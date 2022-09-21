561bf296487f4afb0721411335709d839aaa9e72
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 18:56:07 +0300

    Mimicking a type writer

--- controlling_the_keyboard.py
+++ controlling_the_keyboard.py
@@ -1,6 +1,9 @@
 import pyautogui, time
 
 time.sleep(5)
-pyautogui.typewrite('# This is some stupid comment')
+# pyautogui.click( , );
 
-# This is some stupid comment
+# Trying to cheat on a typing test on https://10fastfingers.com/ my current speed is 60 and this gives a speed of 200 with 100% accuracy
+text = '''an write down have last then from sometimes get begin study here him young spell get did like far keep being not were them could so why near took watch or good together white quite it's start so below big find any began where air add me would in eye tree most below along sometimes show large tell in big always many what something leave most right show write quite old into into all page thought help new came river should America part took another again being begin form over with have again look go still great between found last who how plant for him Indian had letter got boy put different house once get left be said were come state earth until after sound animal but kind air line father mean know  without each run long will play this answer just other air off city that very point because more much let these two came mean oil land together quick Indian can later use boy study letter boy close line school this to these feet sentence if or work enough light our just home water carry ask who line oil over always long back of head has add been every four tell them follow year watch world that began our food night look next and got group most learn every her both its why four people until off car something more miss been are has little word night family few you when oil sound can end mountain think example three your other eat children only important second number white small write should is time around carry read great follow hear try quite quickly began young give is back city fath'''
+
+pyautogui.typewrite(f'{text}', .05)
