4133c5745e51d2b77a51a77fb29158934aa81a47
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 18:04:29 +0300

    Comparing if pixels match

--- screenshot_with_pyautogui.py
+++ screenshot_with_pyautogui.py
@@ -2,3 +2,8 @@ import pyautogui
 
 img = pyautogui.screenshot()
 print(img)
+print(img.getpixel((50, 200)))
+
+# Seeing if the area matches the pixels
+print(pyautogui.pixelMatchesColor(50, 200, (60, 63, 65)))
+print(pyautogui.pixelMatchesColor(50, 200, (130, 135, 144)))
