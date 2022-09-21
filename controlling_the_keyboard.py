b1086e96dc1714465bfebcf53e872882d2c810ab
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 18:18:28 +0300

    Controlling the keyboard

---
+++ controlling_the_keyboard.py
@@ -0,0 +1,6 @@
+import pyautogui, time
+
+time.sleep(5)
+pyautogui.typewrite('# This is some stupid comment')
+
+# This is some stupid comment
--- screenshot_with_pyautogui.py
+++ screenshot_with_pyautogui.py
@@ -10,4 +10,6 @@ print(pyautogui.pixelMatchesColor(50, 200, (130, 135, 144)))
 
 # Clicking a specific position from a taken screenshot
 print(pyautogui.locateOnScreen('example-image.jpg'))
-# if image is a even a pixel off, None will be returned
+# if the image is even a pixel off, None will be returned otherwise it will return a Generator object
+# [(643, 745, 70, 29), (1007, 801, 70, 29)] returned if found on several places
+# then pyautogui.center((643, 745, 70, 29)) and  pyautogui.click((678, 759))
