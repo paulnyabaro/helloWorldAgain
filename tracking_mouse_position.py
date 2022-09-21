9a4d98dccd5fd70b96af720be27fb45795ac568b
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 08:22:36 +0300

    Added image art

--- tracking_mouse_position.py
+++ tracking_mouse_position.py
@@ -1,4 +1,4 @@
-import pyautogui
+import pyautogui, random
 
 # try:
 #     while True:
@@ -12,11 +12,14 @@ import pyautogui
 #
 
 # Clicking the mouse
-pyautogui.click(100, 150, button='right') # middle, left, secondary, primary
-# Another option is pyautogui.mouseDown() and pyautogui.mouseUp()
+# pyautogui.click(100, 150, button='right') # middle, left, secondary, primary
+# Another option is pyautogui.mou![](transparentImage.png)seDown() and pyautogui.mouseUp()
 # Others include
 # pyautogui.doubleClick() , pyautogui.rightClick() and pyautogui.middleClick()
 
 # Dragging the mouse
-pyautogui.dragTo()
-pyautogui.dragRel()
+pyautogui.click(150,150, duration=0.25)
+for i in range(100):
+    pyautogui.dragTo(random.randint(150, 800), random.randint(150, 800), duration=0.25)
+pyautogui.dragTo(150,150, duration=0.25)
+# pyautogui.dragRel()
