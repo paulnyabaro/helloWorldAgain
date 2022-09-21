fb39b3679a423b2231ba6b2a4f312c181bd48b48
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 10:10:14 +0300

    Drawing art

--- drawing_art.py
+++ drawing_art.py
@@ -2,17 +2,30 @@ import pyautogui, time
 time.sleep(2)
 pyautogui.click()
 # click to put drawing program in focus
-distance = 200
-# pyautogui.click(300, 400, duration=0.25)
-# pyautogui.click(510, 400, duration=0.25)
-# pyautogui.click(300, 190, duration=0.25)
-pyautogui.click(510, 190, duration=0.25)
-while distance > 0:
-    pyautogui.dragRel(distance, 0, duration=0.2)
-    # move right
-    distance = distance - 5
-    pyautogui.dragRel(0, distance, duration=0.2)
-    # move down
-    pyautogui.dragRel(-distance, 0, duration=0.2) # move left
-    distance = distance - 5
-    pyautogui.dragRel(0, -distance, duration=0.2) # move up
+
+
+
+def draw_triangle():
+    distance = 200
+    while distance > 0:
+        pyautogui.dragRel(distance, 0, duration=0.9)
+        # move right
+        distance = distance - 5
+        pyautogui.dragRel(0, distance, duration=0.9)
+        # move down
+        pyautogui.dragRel(-distance, 0, duration=0.9)  # move left
+        distance = distance - 5
+        pyautogui.dragRel(0, -distance, duration=0.9)  # move up
+
+
+pyautogui.click(300, 400, duration=0.25)
+draw_triangle()
+time.sleep(10)
+pyautogui.click(500, 400, duration=0.25)
+draw_triangle()
+time.sleep(10)
+pyautogui.click(300, 200, duration=0.25)
+draw_triangle()
+time.sleep(10)
+pyautogui.click(500, 200, duration=0.25)
+draw_triangle()
