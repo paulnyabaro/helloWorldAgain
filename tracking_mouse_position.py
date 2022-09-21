1c8b319a0540bb7b974a4e92e1094f87ede17ff9
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 07:41:26 +0300

    Clicking the mouse

--- tracking_mouse_position.py
+++ tracking_mouse_position.py
@@ -1,12 +1,15 @@
 import pyautogui
 
-try:
-    while True:
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr)
-        # print(len(positionStr), end='', flush=True)
+# try:
+#     while True:
+#         x, y = pyautogui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#         print(positionStr)
+#         # print('\b' * len(positionStr), end='', flush=True)
+#
+# except KeyboardInterrupt:
+#     print('\nDone')
+#
 
-except KeyboardInterrupt:
-    print('Done')
-
+# Clicking the mouse
+pyautogui.click(100, 150, button='right')
