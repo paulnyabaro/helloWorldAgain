b92cc09dc5eee305ea69b9c6afc90d557172204d
barrowpaul <barrowpaul738@gmail.com>
2022-09-21 18:11:06 +0300

    Finding a section of the screenshot from the stored image

--- screenshot_with_pyautogui.py
+++ screenshot_with_pyautogui.py
@@ -7,3 +7,7 @@ print(img.getpixel((50, 200)))
 # Seeing if the area matches the pixels
 print(pyautogui.pixelMatchesColor(50, 200, (60, 63, 65)))
 print(pyautogui.pixelMatchesColor(50, 200, (130, 135, 144)))
+
+# Clicking a specific position from a taken screenshot
+print(pyautogui.locateOnScreen('example-image.jpg'))
+# if image is a even a pixel off, None will be returned
