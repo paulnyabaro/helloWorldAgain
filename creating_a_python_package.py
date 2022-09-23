cd07eaad75fe2387d1a3ca21a050b3919b740835
barrowpaul <barrowpaul738@gmail.com>
2022-09-23 06:14:23 +0300

    Publishing a package to test pypi

--- creating_a_python_package.py
+++ creating_a_python_package.py
@@ -16,3 +16,15 @@
 # README.rst/README.md, license.txt, MANIFEST.in, <package>, data, tests
 # installing local package
 # pip install <path to package>
+
+# Publishing a package to Test PyPI
+# In
+# addition to the account with Test PyPI, we also need to add an API token to the account.
+# We will leave the details of creating an account and adding an API token to the account
+# for you by following the instructions available on the Test PyPI website (https://
+# test.pypi.org/).
+# To push the package to Test PyPI, we will need the Twine utility. We assume Twine is
+# installed using the pip utility. To upload the masifutilv2 package, we will execute the
+# following steps
+# 1. python setup.py sdist
+# 2. twine upload --repository testpypi dist/masifutilv2-0.1.0.tar.gz
