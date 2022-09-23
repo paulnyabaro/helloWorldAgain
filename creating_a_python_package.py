85f79a3c2d59b9a87560b7e521edd1d2d13f8463
barrowpaul <barrowpaul738@gmail.com>
2022-09-23 06:10:10 +0300

    Creating a python package

---
+++ creating_a_python_package.py
@@ -0,0 +1,18 @@
+# It should have
+# setup.py, setup.cfg,
+# setup(
+    # name='package_name',
+    #version='',
+    # description,
+    # URL,
+    # licence = 'LICENCE.txt',
+    # Author,
+    # Author_email,
+    # packages = ['package1', 'package2'],
+    # python_requirements = '>3.5, <4 ',
+    # install_requirements = [
+    # ],
+# ),
+# README.rst/README.md, license.txt, MANIFEST.in, <package>, data, tests
+# installing local package
+# pip install <path to package>
