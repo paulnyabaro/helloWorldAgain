427b5ce4dd0e457c97b0655d40bb0fe345593943
barrowpaul <barrowpaul738@gmail.com>
2022-09-24 05:43:48 +0300

    Special method'

--- oop_in_python.py
+++ oop_in_python.py
@@ -6,11 +6,17 @@ class Car:
         self.i_model = model
         self.i_year = year
 
+    # Special methods
+    def __str__(self):
+        return f"car with color {self.i_model} and \ mileage {self.i_year}"
+
+    # Class method
     @classmethod
     def print_units(cls):
         print(f'The mileage units: {cls.c_mileage_units}')
         print(f'Class name: {cls.__name__}')
 
+    # Static method
     @staticmethod
     def print_hello():
         print('Hello from a static method')
@@ -28,7 +34,7 @@ print(car.i_model)
 print(car.i_color)
 Car.print_units()
 Car.print_hello()
-#
+
 # Important note
 # Attribute names start with c and i to indicate that they are class and instance
 # variables, respectively, and not regular local or global variables. The name of
