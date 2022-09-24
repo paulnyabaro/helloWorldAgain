a5a9be25e64a6b8f9084ff455814738c33c98c9d
barrowpaul <barrowpaul738@gmail.com>
2022-09-24 06:03:39 +0300

    Private and protected variables

--- oop_in_python.py
+++ oop_in_python.py
@@ -1,40 +1,40 @@
-class Car:
-    # Class attributes
-    c_mileage_units = "Mi"
-    # Constructor
-    def __init__(self, model, year):
-        self.i_model = model
-        self.i_year = year
-
-    # Special or dunder methods or rarely magic methods
-    def __str__(self):
-        return f"car with color {self.i_model} and \ mileage {self.i_year}"
-
-    # Class method
-    @classmethod
-    def print_units(cls):
-        print(f'The mileage units: {cls.c_mileage_units}')
-        print(f'Class name: {cls.__name__}')
-
-    # Static method
-    @staticmethod
-    def print_hello():
-        print('Hello from a static method')
-
-    # Destructor
-    def __del__(self):
-        print("Object has been deleted")
-
-car = Car('Toyota', 2010)
-# Instant attributes
-car.i_color = 'red'
-car.i_miles = 10000
-
-print(car.i_model)
-print(car.i_color)
-Car.print_units()
-Car.print_hello()
-print(car) # This will print the content in the __str__ method
+# class Car:
+#     # Class attributes
+#     c_mileage_units = "Mi"
+#     # Constructor
+#     def __init__(self, model, year):
+#         self.i_model = model
+#         self.i_year = year
+#
+#     # Special or dunder methods or rarely magic methods
+#     def __str__(self):
+#         return f"car with color {self.i_model} and \ mileage {self.i_year}"
+#
+#     # Class method
+#     @classmethod
+#     def print_units(cls):
+#         print(f'The mileage units: {cls.c_mileage_units}')
+#         print(f'Class name: {cls.__name__}')
+#
+#     # Static method
+#     @staticmethod
+#     def print_hello():
+#         print('Hello from a static method')
+#
+#     # Destructor
+#     def __del__(self):
+#         print("Object has been deleted")
+#
+# car = Car('Toyota', 2010)
+# # Instant attributes
+# car.i_color = 'red'
+# car.i_miles = 10000
+#
+# print(car.i_model)
+# print(car.i_color)
+# Car.print_units()
+# Car.print_hello()
+# print(car) # This will print the content in the __str__ method
 
 # Important note
 # Attribute names start with c and i to indicate that they are class and instance
@@ -48,10 +48,12 @@ print(car) # This will print the content in the __str__ method
 class Car:
     """outer class"""
     c_mileage_units = "Mi"
-    def __init__(self, color, miles, eng_size):
+    __max_speed = 200 #Private variable
+    def __init__(self, color, miles, eng_size, model):
         self.i_color = color
         self.i_mileage = miles
         self.i_engine = self.Engine(eng_size)
+        self._model = model
     def __str__(self):
         return f"car with color {self.i_color}, mileage \ {self.i_mileage} and engine of {self.i_engine}"
     class Engine:
