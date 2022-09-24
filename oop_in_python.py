4e164b96211627a11061a4343dc7bc7ee709421b
barrowpaul <barrowpaul738@gmail.com>
2022-09-24 06:57:59 +0300

    Abstract class and methods

--- oop_in_python.py
+++ oop_in_python.py
@@ -96,3 +96,31 @@ if __name__ == "__main__":
     # @mileage.setter
     # def mileage(self, new_mil):
     #     self.__mileage = new_mil
+    # Instance name followed by a dot and the attribute name (the Pythonic way)
+
+
+    # Abstract class and methods
+    from abc import ABC, abstractmethod
+
+    class Vehicle(ABC):
+        def hello(self):
+            print(f"Hello from abstract class")
+
+    @abstractmethod
+    def print_me(self):
+        pass
+
+    class Car(Vehicle):
+        def __init__(self, color, seats):
+            self.i_color = color
+            self.i_seats = seats
+    """It is must to implemented this method"""
+
+
+    def print_me(self):
+        print(f"Car with color {self.i_color} and no of \
+    seats {self.i_seats}")
+
+    car = Car("blue", 5)
+    car.print_me()
+    car.hello()
