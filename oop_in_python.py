d10035625e2a872beebb6820237672cea17fabf6
barrowpaul <barrowpaul738@gmail.com>
2022-09-24 05:06:30 +0300

    Added class attributes

--- oop_in_python.py
+++ oop_in_python.py
@@ -1,9 +1,13 @@
 class Car:
-    pass
+    def __init__(self, c_model, year):
+        self.c_model = c_model
+        self.year = year
 
-car = Car()
 
+car = Car('Toyota', 2010)
+# Instant attributes
 car.color = 'red'
 car.miles = 10000
 
+print(car.c_model)
 print(car.color)
