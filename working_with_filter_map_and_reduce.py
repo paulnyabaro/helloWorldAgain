00e15fc48388262329660aae1dfd8950589b8264
barrowpaul <barrowpaul738@gmail.com>
2022-09-28 07:32:47 +0300

    Using map

--- working_with_filter_map_and_reduce.py
+++ working_with_filter_map_and_reduce.py
@@ -12,3 +12,11 @@ mylist1 = [1, 2, 3, 4, 5]
 mylist2 = [6, 7, 8, 9]
 new_list = list(map(product, mylist1, mylist2))
 print(new_list)
+
+
+# Filter
+def is_even(num):
+    return (num % 2 == 0)
+mylist = [1, 2, 3, 4, 5,6,7,8,9]
+new_list = list(filter(is_even, mylist))
+print(new_list)
