da64ee302b7e2b6d8cbadb24ffafcf2c607c7d42
barrowpaul <barrowpaul738@gmail.com>
2022-09-28 08:28:28 +0300

    Set comprehensions

--- workind_with_comprehensions.py
+++ workind_with_comprehensions.py
@@ -11,3 +11,8 @@ print(list3)
 dict = {'a': 21, 'b': 23, 'c': 42, 'd': 68, 'e': 123, 'f': 153}
 dict2 = {x:int(y/2) for (x, y) in dict.items() if y <=200}
 print(dict2)
+
+# Set comprehensions
+list1 = [1, 2, 6, 4, 5, 6, 7, 8, 9, 10, 8]
+set1 = {x for x in list1 if x % 2 == 0}
+print(set1)
