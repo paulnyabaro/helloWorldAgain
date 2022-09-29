ba46341f5b9e0f19057e944dd4893b17dfd9bd98
barrowpaul <barrowpaul738@gmail.com>
2022-09-29 18:19:31 +0300

    Using the asyncio module

---
+++ using_asyncio.py
@@ -0,0 +1,10 @@
+import asyncio
+import time
+async def say(delay, msg):
+    await asyncio.sleep(delay)
+    print(msg)
+
+print("Started at ", time.strftime("%X"))
+asyncio.run(say(1,"Good"))
+asyncio.run(say(2, "Morning"))
+print("Stopped at ", time.strftime("%X"))
