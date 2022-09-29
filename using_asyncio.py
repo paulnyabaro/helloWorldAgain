7651927a0f6e959a1fda0fae8cb33f07484b6e1e
barrowpaul <barrowpaul738@gmail.com>
2022-09-29 18:20:26 +0300

    Using the asyncio module

--- using_asyncio.py
+++ using_asyncio.py
@@ -8,3 +8,7 @@ print("Started at ", time.strftime("%X"))
 asyncio.run(say(1,"Good"))
 asyncio.run(say(2, "Morning"))
 print("Stopped at ", time.strftime("%X"))
+
+# We used the asyncio.sleep function instead of the traditional time.sleep
+# function. If the time.sleep function is used, control will not be given back
+# to the event loop. That is why it is important to use the compatible asyncio.
