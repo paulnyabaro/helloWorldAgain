26ff6405f582a93d8388593f72f7fb30f7b962e5
barrowpaul <barrowpaul738@gmail.com>
2022-10-02 20:09:34 +0300

    Some simple practice

--- practise_1.py
+++ practise_1.py
@@ -1,6 +1,10 @@
-import requests, webbrowser
+import requests, webbrowser, pyautogui, time
 
-url = 'https://google.com'
+url = 'https://b-ok.africa/'
 open_google = requests.get(url)
 print(open_google)
 webbrowser.open(url)
+
+time.sleep(5)
+pyautogui.typewrite('Things fall apart', 0.2)
+pyautogui.press('enter')
