97881b3be3db5a071bded3427c207f9cb1f7d044
barrowpaul <barrowpaul738@gmail.com>
2022-10-03 07:25:05 +0300

    Flask sample code

--- flask_sample_code.py
+++ flask_sample_code.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, render_template, redirect
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    grade = db.Column(db.String(20), nullable=True)
-def __repr__(self):
-    return '<Student %r>' % self.name
+# from flask import Flask, request, render_template, redirect
+# from flask_sqlalchemy import SQLAlchemy
+#
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+# class Student(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+#     grade = db.Column(db.String(20), nullable=True)
+# def __repr__(self):
+#     return '<Student %r>' % self.name
