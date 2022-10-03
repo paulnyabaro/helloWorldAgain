0f9710c919495b55c96a744897fc02c122ceaa04
barrowpaul <barrowpaul738@gmail.com>
2022-10-03 08:05:17 +0300

    Fetching and deleting data from DB

--- REST_API_sample_code.py
+++ REST_API_sample_code.py
@@ -2,3 +2,14 @@
 
 # Working with RESTful in flask
 # pip install Flask-RESTful
+#
+# Fetching and deleting data from db
+# class StudentDao(Resource):
+#     def get(self, student_id):
+#         student = Student.query.filter_by(id=student_id).first_or_404(description='Record with id={} is not available'.format(student_id))
+#     return student.serialize()
+#     def delete(self, student_id):
+#         student = Student.query.filter_by(id=student_id).first_or_404(description='Record with id={} is not available'.format(student_id))
+#     db.session.delete(student)
+#     db.session.commit()
+#     return '', 204
