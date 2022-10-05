f88e1f3381d23595e50e3f5d6fc86f32a2065e12
barrowpaul <barrowpaul738@gmail.com>
2022-10-05 10:06:40 +0300

    Saving and loading model data from a file

--- machine_learning_content.py
+++ machine_learning_content.py
@@ -15,3 +15,13 @@
 #     x_new = [[5.6, 2.6, 3.9, 1.2]]
 #     y_new = loaded_model.predict(x_new)
 #     print("X=%s, Predicted=%s" % (x_new[0], y_new[0]))
+
+
+# #iris_save_load_predict_gridmodel.py
+# #grid_cv is created and trained using the code in the
+# #iris_eval_svm_model.py
+# joblib.dump(grid_cv.best_estimator_, "model.joblib")
+# loaded_model = joblib.load("model.joblib")
+# x_new = [[5.6, 2.5, 3.9, 1.1]]
+# y_new = loaded_model.predict(x_new)
+# print("X=%s, Predicted=%s" % (x_new[0], y_new[0]))
