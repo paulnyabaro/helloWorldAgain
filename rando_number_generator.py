5d57d5d265faff6a76a44520c814e86bbbca3e01
paulnyabaro <paulnyabaro1@gmail.com>
2022-12-31 12:18:34 +0300

    Updated random symbols generator;

--- rando_number_generator.py
+++ rando_number_generator.py
@@ -1,12 +1,11 @@
 from random import randint
 final_number = ''
 games_dictionary = {
-    '1':'1',
-    '2':'X',
-    '3':'2' 
+    '1':'Y',
+    '2':'N',
 }
-for i in range(10):
-    random_key = str((randint(1,3)))
+for i in range(13):
+    random_key = str((randint(1,2)))
     final_number += games_dictionary[random_key]
 
 print (final_number)
